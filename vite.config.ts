0d3579c0a0084ed3c02ca4c06a93ad50f418ce1a
GhostGoju <129934609+GhostGoju@users.noreply.github.com>
2025-07-10 13:15:26 -0500

    Ajustes de importaciones

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Portafolio/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // ← 🔥 usa path.resolve
+    },
+  },
 })
